60eb6c95ed385d9a127ceb7dc14eeb23012161ee
sKisiangani <80091429+devopsengineersam@users.noreply.github.com>
2025-08-04 08:26:32 -0500

    added helm and gemini files

--- others/command.sh
+++
@@ -1,4 +0,0 @@
-gcloud projects add-iam-policy-binding gfbiuser-zwer-48ot-y44d-0xlp7b \
---member="serviceAccount:dummy-sa@gfbiuser-zwer-48ot-y44d-0xlp7b.iam.gserviceaccount.com" \
---role="projects/gfbiuser-zwer-48ot-y44d-0xlp7b/roles/eis_sectools_scoped_role" \
---condition="expression=resource.type == 'iam.googleapis.com/Role' && resource.service == 'iam.googleapis.com' && resource.name.extract('/roles/{role_id}').startsWith('dig-'),title=Restrict IAM Role Management to 'dig-' prefixed roles on IAM service"
